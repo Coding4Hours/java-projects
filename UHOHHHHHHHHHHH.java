1bb35c628bdfa197e31fac611b61a36a24b5790f
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2023-12-11 16:24:06 -0800

    a

---
+++ UHOHHHHHHHHHHH.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+public class UHOHHHHHHHHHHH {
+	public static void s(String[] args) throws InterruptedException {
+		int randomNum = (int) (Math.random() * 1000000001);
+		int ranomNum = (int) (Math.random() * 1000000001);
+		int randmNm = (int) (Math.random() * 1000000001);
+		int randoN = (int) (Math.random() * 1000000001);
+		int rando = (int) (Math.random() * 1000000001);
+		int rand = (int) (Math.random() * 1000000001);
+		int ran = (int) (Math.random() * 1000000001);
+		int ra = (int) (Math.random() * 1000000001);
+		System.out.print(randomNum);
+		System.out.print(ranomNum);
+		System.out.print(randmNm);
+		System.out.println(randoN);
+		System.out.print(rando);
+		System.out.print(rand);
+		System.out.print(ran);
+		System.out.print(ra);
+		System.out.println();
+		Scanner uhoh = new Scanner(System.in);
+		String words = uhoh.nextLine();
+		System.out.println("3");
+		TimeUnit.SECONDS.sleep(1);
+		System.out.println("2");
+		TimeUnit.SECONDS.sleep(1);
+		System.out.println("1");
+		TimeUnit.SECONDS.sleep(1);
+		System.out.println("UHOH!!!");
+		TimeUnit.SECONDS.sleep(1);
+		while (ra > 0) {
+			System.out.println(words);
+		}
+		System.out.println(uhoh);
+	}
+}
