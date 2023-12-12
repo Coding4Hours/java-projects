bf7dd5d1ceb313bfe423eb1e1ce8e4a22c0dc204
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2023-12-11 16:20:40 -0800

    a

---
+++ join_method.java
@@ -0,0 +1,15 @@
+public class netjstech{
+
+ public static void main(String[] args) {
+	  String str1 = "An";
+	  String str2 = "example";
+	  String str3 = "string";
+	  
+	  // joining with space
+	  String finalStr = String.join(" ", str1, str2, str3);
+	  System.out.println("str - " + finalStr);
+	  // joining with hyphen
+	  finalStr = String.join("-", str1, str2, str3);
+	  System.out.println("str - " + finalStr);	
+ }
+}
