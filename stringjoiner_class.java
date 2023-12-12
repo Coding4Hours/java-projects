62904bc54ca62096d5383ea352a2c8b53b04f333
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2023-12-11 16:37:12 -0800

    Update stringjoiner_class.java

--- stringjoiner_class.java
+++ stringjoiner_class.java
@@ -12,3 +12,28 @@ public class StringJoinerDemo {
   System.out.println(sj.toString());
  }
 }
+
+class StringJoinerDemo2 {
+ public static void main(String[] args) {
+  StringJoiner sj = new StringJoiner(":", "[", "]");
+  System.out.println("StringJoiner- " + sj.toString());
+  sj.setEmptyValue("No element is added yet");
+  System.out.println("StringJoiner- " + sj.toString());
+  sj.add("Adding").add("Values").add("Now");
+  System.out.println("StringJoiner- " + sj.toString());
+ }
+}
+
+class StringJoinerDemo3 {
+ public static void main(String[] args) {
+  StringJoiner sj1 = new StringJoiner(":", "[", "]");
+  sj1.add("This").add("is").add("First").add("String");
+  System.out.println("Length of sj1- " + sj1.length());
+  StringJoiner sj2 = new StringJoiner("-", "{", "}");
+  sj2.add("Second").add("String");
+  System.out.println("Length of sj2- " + sj2.length());
+  sj1 = sj1.merge(sj2);
+  System.out.println("Merged String- " + sj1.toString());
+  System.out.println("Length of Merged String- " + sj1.length());
+ }
+}
